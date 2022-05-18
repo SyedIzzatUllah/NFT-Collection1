7f3d72d884d9e7296e3e17a9117a41f577deb2ce
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-05-18 22:14:26 +0500

    Update deploy.js

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -6,7 +6,7 @@ async function main() {
   // Address of the whitelist contract that you deployed in the previous module
   const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
   // URL from where we can extract the metadata for a Crypto Dev NFT
-  const metadataURL = METADATA_URL;
+  const metadataURL = METADATA_URL;   
   /*
   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
   so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
