5916896a596cad067a7a757b9feb3a2f9f1f81d7
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-09-07 21:13:47 +0500

    Update deploy.js

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -21,7 +21,7 @@ async function main() {
 
   // print the address of the deployed contract
   console.log(
-    "Crypto Devs Contract Address:",
+    "Crypto Contract Address:", 
     deployedCryptoDevsContract.address
   );
 }
